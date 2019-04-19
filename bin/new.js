2e2dd15ce7ef4f95e84710a74d213fe844d35a5c
Carlos Cabrera <cacabrer@adobe.com>
2019-04-19 15:59:24 -0500

    Simplifying!

--- bin/new.js
+++ bin/new.js
@@ -1,11 +1,9 @@
 #! /usr/bin/env node
-
 const [ _, _bin, command, name, ...attr ] = process.argv
 
 switch(command) {
     case 'component':
         return require('./lib/new-component')(name)
     default:
-        console.log('What was that?')
         return
 }
