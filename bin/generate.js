7690a48377e1862be2cc9c360d52c8add33db1e6
Carlos Cabrera <cacabrer@adobe.com>
2019-04-25 08:15:53 -0500

    CLI Usage

--- bin/generate.js
+++ bin/generate.js
@@ -1,9 +1,22 @@
 #! /usr/bin/env node
 const [ _, _bin, command, name, ...attr ] = process.argv
 
+const usage = `
+🏗  \x1b[4mStorybook Generator\x1b[0m
+
+    → yarn generate <action> <ComponentName>
+
+    Actions:
+    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    component: Generates a new UI Component
+        ↳ yarn generate component <ComponentName>
+
+`
+
+
 switch(command) {
     case 'component':
         return require('./lib/new-component')(name)
     default:
-        return
+        return console.log(usage)
 }
