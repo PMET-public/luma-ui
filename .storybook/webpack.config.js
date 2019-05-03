0437d308456c71b7be39d51aa07ef66c4684c17f
Carlos Cabrera <cacabrer@adobe.com>
2019-05-03 17:07:10 -0500

    Cleaned up syntax

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,13 +2,15 @@ const path = require('path')
 
 module.exports = ({ config }) => {
 
-    config.module.rules.push({
-        test: /\.tsx?$/,
-        use: [
-            'awesome-typescript-loader',
-        ],
-        exclude: /node_modules/,
-    })
+    config.module.rules.push(
+        {
+            test: /\.tsx?$/,
+            use: [
+                'awesome-typescript-loader',
+            ],
+            exclude: /node_modules/,
+        },
+    )
 
     config.resolve.extensions.push('.ts', '.tsx')
 
