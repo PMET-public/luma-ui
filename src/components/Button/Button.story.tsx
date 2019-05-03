5a4ae60023480b34ccbbffbf70246d6a8002ab83
Carlos Cabrera <cacabrer@adobe.com>
2019-05-03 17:08:02 -0500

    Added actions

--- src/components/Button/Button.story.tsx
+++ src/components/Button/Button.story.tsx
@@ -3,20 +3,23 @@ import Button from './'
 import centered from '@storybook/addon-centered/react'
 import { text, select, boolean } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 const types: any = {
-    button: 'button', 
-    reset: 'reset', 
+    button: 'button',
+    reset: 'reset',
     submit: 'submit'
 }
 
 storiesOf('Components/Button', module)
     .addDecorator(centered)
     .add('React', () => (
-        <Button 
-            disabled={ boolean('disabled', false, 'props') }
-            type={ select('type', types, 'button', 'props') }
+        <Button
+            disabled={boolean('disabled', false, 'props')}
+            type={select('type', types, 'button', 'props')}
+            onClick={action('onClick')}
         >
-            { text('children', 'Label', 'props') }
+            {text('children', 'Label', 'props')}
         </Button>
     ))
+
