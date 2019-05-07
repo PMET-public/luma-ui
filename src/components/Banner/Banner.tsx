ce606d7c022a392b1511902bc87a6cde1843c235
Carlos Cabrera <cacabrer@adobe.com>
2019-05-07 09:14:55 -0500

    BEM class names conventions

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,14 +4,14 @@ type BannerProps = {}
 
 export const Banner: FunctionComponent<BannerProps> = ({ }) => (
     <Fragment>
-        <div className="banner-wrapper">
-            <div className="content">
-                <div className="overlay">
+        <div className="banner">
+            <div className="banner__content">
+                <div className="banner__content__overlay">
                     <h1>Headline</h1>
                     <h2>Subheadline</h2>
                 </div>
             </div>
-            <button>Button</button>
+            <button className="banner__button">Button</button>
         </div>
     </Fragment>
 )
