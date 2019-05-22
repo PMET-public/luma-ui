cc2dffbe5c8824f8ab97923d61f2ab0f03a0aff7
Carlos Cabrera <cacabrer@adobe.com>
2019-05-22 13:23:22 -0500

    fixed typo

--- src/components/CodeBlock/index.ts
+++ src/components/CodeBlock/index.ts
@@ -1,3 +1,3 @@
 import './CodeBlock.css'
-export * from './ClodeBlock'
-export { CodeBlock as default } from './ClodeBlock'
+export * from './CodeBlock'
+export { CodeBlock as default } from './CodeBlock'
