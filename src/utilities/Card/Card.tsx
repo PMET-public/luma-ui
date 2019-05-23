d96440665d5ab8c5d31fbc6757204cc97772e36f
Carlos Cabrera <cacabrer@adobe.com>
2019-05-23 15:49:57 -0500

    Fix undefined className

--- src/utilities/Card/Card.tsx
+++ src/utilities/Card/Card.tsx
@@ -2,10 +2,10 @@ import React, { FunctionComponent, forwardRef } from 'react'
 
 export type CardProps = any
 
-export const Card: FunctionComponent<CardProps> = forwardRef(({ children, className, ...props }, ref) => (
+export const Card: FunctionComponent<CardProps> = forwardRef(({ children, className = '', ...props }, ref) => (
     <div 
         {...props}
-        className={`card ${className}`} 
+        className={`card ${className}`}
         ref={ref}
     >{children}</div>
 ))
