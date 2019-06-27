7a215de9015144951f25031ea1b0c06242d732b5
Carlos Cabrera <cacabrer@adobe.com>
2019-06-27 11:18:17 -0500

    Updated template to a most used case

--- bin/templates/components/Component.js
+++ bin/templates/components/Component.js
@@ -4,23 +4,23 @@ module.exports = (Name, filename) => source`
 
     import React, { FunctionComponent } from 'react'
 
-    export type ${Name}Props = { 
+    export type ${Name}Props = { }
 
+    export const ${Name}: FunctionComponent<${Name}Props> = ({ }) => {
+        return (
+            <div className="${filename}">
+                👋 Hi, I'm ${Name}
+
+                <style jsx>{\`
+                    .${filename} {
+                        padding: 2rem;
+                        background: white;
+                        border-radius: 1rem;
+                        box-shadow: 0 0 1rem #ccc;
+                    }
+                \`}</style>
+            </div>
+        )
     }
-
-    export const ${Name}: FunctionComponent<${Name}Props> = ({ }) => (
-        <div className="${filename}">
-            👋 Hi, I'm ${Name}
-
-            <style jsx>{\`
-                .${filename} {
-                    padding: 2rem;
-                    background: white;
-                    border-radius: 1rem;
-                    box-shadow: 0 0 1rem #ccc;
-                }
-            \`}</style>
-        </div>
-    )
 
 ` + '\n'
