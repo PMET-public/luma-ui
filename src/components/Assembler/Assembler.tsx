c907307b4bec7c67e8db1b35d0f4c76784c3d22e
Carlos Cabrera <cacabrer@adobe.com>
2019-07-26 14:03:11 -0500

    Fixed Asssembler to only load Components when the component attribute is updated

--- src/components/Assembler/Assembler.tsx
+++ src/components/Assembler/Assembler.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState, ReactNode } from 'react'
 import { Component, Props, Element, classes, ErrorBoundary } from '../../lib'
 
 export type AssemblerProps = Props<{
@@ -20,37 +20,43 @@ export const Assembler: Component<AssemblerProps> = ({
     components = [],
     ...props
 }) => {
-
-    return (
-        <Element {...props} className={classes('assembler')}>
+    const [children, setChildren] = useState<ReactNode>(null)
+    
+    useEffect(() => {
+        setChildren(
             <Suspense fallback={<Loading />}>
                 {components.map(({ name, props }, index) => {
-                    const Component = React.lazy(() => import(`../${name}`))
+                    const DynamicComponent = React.lazy(() => import(`../${name}`))
                     return (
                         <React.Fragment key={index}>
                             <ErrorBoundary>
-                                <Component {...props} />
+                                <DynamicComponent {...props} />
                             </ErrorBoundary>
                         </React.Fragment>
                     )
                 })}
-
-                <style jsx global>{`
-                    .assembler {
-                        --gap: 3rem;
-                        display: grid;
-                        padding-top: 2rem;
-                        padding-bottom: 2rem;
-                        grid-gap: var(--gap);
-                        grid-auto-columns: minmax(0, 1fr);
-                        grid-auto-rows: minmax(max-content, max-content);
-
-                        @media(--medium-screen) {
-                            --gap: 4rem;
-                        }
-                    }
-                `}</style>
             </Suspense>
+        )
+    }, [components])
+    return (
+        <Element {...props} className={classes('assembler')}>
+            {children}
+
+            <style jsx global>{`
+                .assembler {
+                    --gap: 3rem;
+                    display: grid;
+                    padding-top: 2rem;
+                    padding-bottom: 2rem;
+                    grid-gap: var(--gap);
+                    grid-auto-columns: minmax(0, 1fr);
+                    grid-auto-rows: minmax(max-content, max-content);
+
+                    @media(--medium-screen) {
+                        --gap: 4rem;
+                    }
+                }
+            `}</style>
         </Element>
     )
 }
