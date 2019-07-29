40b4c3b3e2227bdb5e6a5c96d56debf6937497c8
Carlos Cabrera <cacabrer@adobe.com>
2019-07-29 14:18:19 -0500

    Swatches components

---
+++ src/components/Swatches/Swatches.story.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Swatches from '.'
+import { storiesOf } from '@storybook/react'
+import { object } from '@storybook/addon-knobs'
+
+storiesOf('📦 Components/Swatches', module)
+    .add('Default', () => (
+        <div className="story">
+            <div className="story__wrapper">
+                <Swatches items={object('list', [
+                    { label: 2, as: 'button', disabled: true },
+                    { label: 4, active: true , as: 'button'},
+                    { label: 6 },
+                    { label: 8 },
+                ])}/>
+            </div>
+            <style jsx global>{`
+                .story {
+                    align-items: center;
+                    display: flex;
+                    height: 100vh;
+                    justify-content: center;
+                    width: 100%;
+                }
+                
+                .story__wrapper {
+                    width: 30rem;
+                }
+            `}</style>
+        </div>
+    ))
---
+++ src/components/Swatches/index.ts
@@ -0,0 +1,2 @@
+export * from './Swatches'
+export { Swatches as default } from './Swatches'
