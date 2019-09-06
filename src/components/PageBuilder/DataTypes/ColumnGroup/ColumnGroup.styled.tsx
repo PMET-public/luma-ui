14991f4281ef483ee3bc09a6012412fc52299a82
Carlos A. Cabrera <cacabrer@adobe.com>
2019-09-06 14:18:12 -0500

    column responsive

--- src/components/PageBuilder/DataTypes/ColumnGroup/ColumnGroup.styled.tsx
+++ src/components/PageBuilder/DataTypes/ColumnGroup/ColumnGroup.styled.tsx
@@ -1,5 +1,16 @@
 import styled from 'styled-components'
 
+import { Root as ColumnRoot } from '../Column'
+
 export const Root = styled.div`
     display: flex;
+    flex-direction: row;
+
+    @media ${props => props.theme.breakpoints.smallOnly} {
+        flex-direction: column;
+
+        ${ColumnRoot} {
+            width: 100% !important;
+        }
+    }
 `
