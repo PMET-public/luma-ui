2fe6e2e5b581e2cfb110c1dfdadf5050a1c2d8b2
Carlos Cabrera <cacabrer@adobe.com>
2019-09-17 09:46:42 -0500

    pagebuilder heading styles

--- src/components/PageBuilder/ContentTypes/Banner/Banner.styled.tsx
+++ src/components/PageBuilder/ContentTypes/Banner/Banner.styled.tsx
@@ -35,6 +35,9 @@ export const Content = styled.div`
     & h3,
     & h4 {
         font-size: 3rem;
+        font-family: ${props => props.theme.typography.heading.family};
+        font-weight: ${props => props.theme.typography.heading.weight};
+
         @media ${props => props.theme.breakpoints.medium} {
             font-size: 4rem;
         }
