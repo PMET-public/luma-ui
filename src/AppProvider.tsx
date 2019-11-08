55ecb35fe34d3b4a1b331bd5247f6e99400119b1
Carlos A. Cabrera <cacabrer@adobe.com>
2019-11-07 23:12:54 -0600

    online flag

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,4 +1,13 @@
-import React, { FunctionComponent, useEffect, createContext, useContext, useReducer, Reducer, Dispatch } from 'react'
+import React, {
+    FunctionComponent,
+    useEffect,
+    createContext,
+    useContext,
+    useReducer,
+    Reducer,
+    Dispatch,
+    useCallback,
+} from 'react'
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import { ResetStyles } from './theme/ResetStyles'
 import { GlobalStyles, Root } from './theme/GlobalStyles'
@@ -10,17 +19,24 @@ export type AppProviderProps = {}
 
 type ReducerState = {
     colorScheme: string
+    online: boolean
 }
 
-type ReducerActions = {
-    type: 'setColorScheme'
-    payload: string
-}
+type ReducerActions =
+    | {
+          type: 'setColorScheme'
+          payload: string
+      }
+    | {
+          type: 'setOnline'
+          payload: boolean
+      }
 
 type AppContextProps = [ReducerState, Dispatch<ReducerActions>]
 
 const initialState: ReducerState = {
     colorScheme: (typeof localStorage !== 'undefined' && localStorage.getItem('luma-ui/colorScheme')) || 'light',
+    online: true,
 }
 
 const reducer: Reducer<ReducerState, ReducerActions> = (state, action) => {
@@ -29,6 +45,11 @@ const reducer: Reducer<ReducerState, ReducerActions> = (state, action) => {
             return {
                 ...state,
                 colorScheme: action.payload,
+            }
+        case 'setOnline':
+            return {
+                ...state,
+                online: action.payload,
             }
         default:
             throw `Reducer action not valid.`
@@ -40,11 +61,33 @@ export const AppContext = createContext<AppContextProps>([initialState, x => {}]
 export const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
+    /**
+     * Save Color Preference
+     */
     useEffect(() => {
         localStorage.setItem('luma-ui/colorScheme', state.colorScheme)
     }, [state.colorScheme])
 
     const colors = colorSchemes[state.colorScheme]
+
+    /**
+     * Offline
+     */
+    const handleNetworkChange = useCallback(() => {
+        dispatch({ type: 'setOnline', payload: navigator.onLine })
+    }, [])
+
+    useEffect(() => {
+        dispatch({ type: 'setOnline', payload: navigator.onLine })
+
+        window.addEventListener('online', handleNetworkChange)
+        window.addEventListener('offline', handleNetworkChange)
+
+        return () => {
+            window.removeEventListener('online', handleNetworkChange)
+            window.removeEventListener('offline', handleNetworkChange)
+        }
+    }, [])
 
     return (
         <AppContext.Provider value={[state, dispatch]}>
