e1a874eb9760c48ff9407a296235f7019af349cd
Carlos A. Cabrera <cacabrer@adobe.com>
2019-11-15 18:48:40 -0600

    not longer needed

--- src/components/Link/Link.story.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Link from './'
-import { storiesOf } from '@storybook/react'
-
-storiesOf('📦 Components/Link', module).add('Default', () => (
-    <Link href="https://magento.com" target="_blank">
-        🛍 Mageto.com
-    </Link>
-))
--- src/components/Link/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Link'
-export * from './Link.styled'
-export { Link as default } from './Link'
