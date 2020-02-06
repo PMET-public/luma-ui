0d5249d2056b94693863a2f75e9c620fac91d39f
Carlos A. Cabrera <cacabrer@adobe.com>
2020-02-06 09:05:51 -0600

    using Form Context

--- src/components/Form/Checkbox/Checkbox.tsx
+++ src/components/Form/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from 'react'
+import React, { HTMLAttributes, useEffect } from 'react'
 import { Component } from '../../../lib'
 import { Wrapper, Input, Item, OffIcon, OnIcon, Placeholder } from './Checkbox.styled'
 import { Field, Label, Error } from '../Form'
@@ -7,10 +7,13 @@ import RadioOnIconSvg from 'remixicon/icons/System/checkbox-circle-line.svg'
 import RadioOffIconSvg from 'remixicon/icons/System/checkbox-blank-circle-line.svg'
 import CheckboxOnIconSvg from 'remixicon/icons/System/checkbox-line.svg'
 import CheckboxOffIconSvg from 'remixicon/icons/System/checkbox-blank-line.svg'
+import { ValidationOptions, useFormContext, ErrorMessage } from 'react-hook-form'
+import _get from 'lodash.get'
 
 export type CheckboxProps = {
     label?: string
-    error?: { message?: string } | boolean
+    error?: string
+    rules?: ValidationOptions
     type?: 'checkbox' | 'radio'
     items: Array<
         {
@@ -20,33 +23,55 @@ export type CheckboxProps = {
     >
 }
 
-export const Checkbox: Component<CheckboxProps> = React.forwardRef(
-    ({ label, as, error, name, placeholder, type = 'checkbox', items, ...props }, ref: any) => {
-        return (
-            <fieldset>
-                <Field as={as} {...props}>
-                    {label && (
-                        <Label as="legend" $error={!!error}>
-                            {label}
-                        </Label>
-                    )}
+export const Checkbox: Component<CheckboxProps> = ({
+    as,
+    error,
+    rules,
+    label,
+    name,
+    placeholder,
+    type = 'checkbox',
+    items,
+    ...props
+}) => {
+    const { register, setError, clearError, errors } = useFormContext()
 
-                    {placeholder && <Placeholder>{placeholder}</Placeholder>}
+    const hasError = !!error || !!_get(errors, name)
 
-                    <Wrapper>
-                        {items.map(({ text, ...item }, index) => (
-                            <Item key={index}>
-                                <Input type={type} name={name} ref={ref} {...item} />
-                                <OffIcon as={type === 'radio' ? RadioOffIconSvg : CheckboxOffIconSvg} />
-                                <OnIcon as={type === 'radio' ? RadioOnIconSvg : CheckboxOnIconSvg} />
-                                {text}
-                            </Item>
-                        ))}
-                    </Wrapper>
+    useEffect(() => {
+        if (error) {
+            setError(name, 'server', error)
+        } else {
+            clearError(name)
+        }
+    }, [error])
 
-                    <Error>{typeof error === 'object' && error.message}</Error>
-                </Field>
-            </fieldset>
-        )
-    }
-)
+    return (
+        <fieldset>
+            <Field as={as} {...props}>
+                {label && (
+                    <Label as="legend" $error={hasError}>
+                        {label}
+                    </Label>
+                )}
+
+                {placeholder && <Placeholder>{placeholder}</Placeholder>}
+
+                <Wrapper>
+                    {items.map(({ text, ...item }, index) => (
+                        <Item key={index}>
+                            <Input type={type} {...item} name={name} ref={register({ ...rules })} />
+                            <OffIcon as={type === 'radio' ? RadioOffIconSvg : CheckboxOffIconSvg} />
+                            <OnIcon as={type === 'radio' ? RadioOnIconSvg : CheckboxOnIconSvg} />
+                            {text}
+                        </Item>
+                    ))}
+                </Wrapper>
+
+                <Error>
+                    <ErrorMessage name={name}>{({ message }) => message}</ErrorMessage>
+                </Error>
+            </Field>
+        </fieldset>
+    )
+}
